41a26ec126046d533808439e4f393ea110abe274
kadri-ayoub-2022 <kadriayoub122@gmail.com>
2024-06-20 14:59:44 +0100

    section 4

--- docker.js
+++ docker.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
+const mongoose = require('mongoose');
+const redis = require('redis');
+
+const REDIS_PORT = 6379;
+const REDIS_HOST = 'redis';
+
+const redisClient = redis.createClient({
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+});
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('connect', () => console.log('connected to redis...'));
+redisClient.connect();
+
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+const DB_HOST = 'mongo';
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI)
+  .then(() => console.log('connected to db...'))
+  .catch((err) => console.log('failed to connect to db: ', err));
 
 app.get('/',(req,res,next)=>{
     res.send('<h1> DOCKER </h1>')
